1e7a178daf830420eee4592d225b029883f3dee6
Eduard Abajyan <edwardabajyan@gmail.com>
2026-06-17 14:56:23 +0400

    first try to improve the image sizing

--- public/js/resizeAboutMeImg.js
+++ public/js/resizeAboutMeImg.js
@@ -1,19 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-  function resizeAboutMeImage() {
-    let containerWidth = document.getElementById("imageAboutMe").offsetWidth;
-    let containerHeight = document.getElementById("imageAboutMe").offsetHeight;
-    let imageWidth = (containerWidth >>> 3) * 7;
-    let imageHeight = (containerHeight >>> 3) * 7;
+function resizeAboutMeImage() {
+  let containerWidth = document.getElementById("imageAboutMe").offsetWidth;
+  let containerHeight = document.getElementById("imageAboutMe").offsetHeight;
+  let imageWidth = (containerWidth >>> 3) * 7;
+  let imageHeight = (containerHeight >>> 3) * 7;
 
-    if (containerWidth * 4 > containerHeight * 3)
-      imageWidth = (imageHeight * 3) >>> 2;
-    else if (containerWidth * 4 < containerHeight * 3)
-      imageHeight = (imageWidth << 2) / 3;
+  if (containerWidth * 4 > containerHeight * 3)
+    imageWidth = (imageHeight * 3) >>> 2;
+  else if (containerWidth * 4 < containerHeight * 3)
+    imageHeight = (imageWidth << 2) / 3;
 
-    let img = document.querySelector("#imageAboutMe .frame img");
-    img.style.width = imageWidth + "px";
-    img.style.height = imageHeight + "px";
-  }
+  let img = document.querySelector("#imageAboutMe .frame img");
+  img.style.width = imageWidth + "px";
+  img.style.height = imageHeight + "px";
+}
+document.addEventListener("DOMContentLoaded", function () {
   resizeAboutMeImage();
   window.addEventListener("resize", resizeAboutMeImage);
 });
