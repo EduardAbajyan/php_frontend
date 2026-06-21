921b05672467905c65492bf7ad2fc49ad13904e6
Eduard Abajyan <edwardabajyan@gmail.com>
2026-06-21 20:53:38 +0400

    nighttime color mode for buttons and the page6

--- app/views/home/caloriesAppPage.php
+++ app/views/home/caloriesAppPage.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="module" src="<?php echo base_url('asset/js/color-mode.js') ?>"></script>
     <link rel="icon" href="data:;" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
